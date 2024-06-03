e93eb4c8bf8a8a018eeff8d2cb9681988f77ed0a
safin878 <shafin6747@gmail.com>
2024-06-04 01:48:06 +0600

    section title component Make

---
+++ src/Pages/AboutBuilding/AboutBuilding.jsx
@@ -0,0 +1,11 @@
+import SectionTitle from "./../../Componenents/SectionTitle/SectionTitle";
+
+const AboutBuilding = () => {
+  return (
+    <div>
+      <SectionTitle Heading={"About The Building"}></SectionTitle>
+    </div>
+  );
+};
+
+export default AboutBuilding;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import { Helmet } from "react-helmet-async";
 import Banner from "../../Componenents/Banner/Banner";
+import AboutBuilding from "../AboutBuilding/AboutBuilding";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
         <title>BuildiFy || Home</title>
       </Helmet>
       <Banner></Banner>
+      <AboutBuilding></AboutBuilding>
     </div>
   );
 };
